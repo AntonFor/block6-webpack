c66184b84c9d14a4bd4113eabca27eef4a4ba2a6
AntonFor <63106309+AntonFor@users.noreply.github.com>
2020-10-18 22:19:32 +0700

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,37 @@ import './services.js';
 import './burger.js';
 import './feedback.js';
 import './call.js';
-import './swiper-bundle.min.js';
+import Swiper from 'swiper';
+
+Swiper.use([Pagination]);
+
+const swiper = new Swiper(slider, {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: false,
+	spaceBetween: 16,
+	slidesPerView: 'auto',
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination--brand',
+		type: 'bullets',
+		clickable: true,
+	},
+
+	breakpoints: {
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 'auto',
+			spaceBetween: 30,
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 'auto',
+			spaceBetween: 40,
+		}
+	}
+});
+
 
 console.log('Works!');
